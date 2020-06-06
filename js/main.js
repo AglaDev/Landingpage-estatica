f63aeca0f90435d69de710d107e1674fc1439db9
ZeAglailson <jaglailson1@gmail.com>
2020-06-06 15:26:38 -0300

    btn menu att

---
+++ js/main.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+
+    //CONFIGURAÇÕES DO MENU
+    var btnMenu = this.document.getElementById('bt-menu');
+
+    btnMenu.addEventListener('click', function(){
+
+        btnMenu.classList.toggle('btn-menu-toggle');
+
+    })
+
+
+
+
+}
